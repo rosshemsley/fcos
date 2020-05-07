a64267cc93446b51364bf14087e29cfc6290394a
Ross Hemsley <rlhemsley@gmail.com>
2020-05-07 12:43:50 +0100

    🗣 remote comments

--- fcos/inference/inference.py
+++ fcos/inference/inference.py
@@ -49,8 +49,6 @@ def compute_detections_for_tensor(model: FCOS, x, device) -> List[Detection]:
         all_boxes = []
 
         for feature_classes, feature_boxes in zip(classes_by_feature, boxes_by_feature):
-
-            # print("Class shape", feature_classes.shape)
             all_classes.append(feature_classes.view(batch_size, -1, len(model.classes)))
             all_boxes.append(feature_boxes.view(batch_size, -1, 4))
 
@@ -69,18 +67,10 @@ def detections_from_net(boxes_by_batch, classes_by_batch, scores_by_batch=None) 
     """
     result = []
 
-    # print("SCORE by batch SHAPE", scores_by_batch.shape)
-    # print("CLASS by batch SHAPE", classes_by_batch.shape)
-    # print("BOXES by batch SHAPE", boxes_by_batch.shape)
-
     for batch in range(classes_by_batch.shape[0]):
         scores = scores_by_batch[batch] if scores_by_batch is not None else None
         classes = classes_by_batch[batch]
         boxes = boxes_by_batch[batch]
-
-        # print("SCORE SHAPE", scores.shape)
-        # print("CLASS SHAPE", classes.shape)
-        # print("BOXES SHAPE", boxes.shape)
 
         result.append(
             [
@@ -97,35 +87,7 @@ def detections_from_net(boxes_by_batch, classes_by_batch, scores_by_batch=None) 
     return result
 
 
-#         # if box_labels is None or class_labels is None:
-#         #     # box_labels = box_labels.to(x.device)
-#         #     # class_labels = class_labels.to(x.device)
-#         #     # boxes: B[ltrb]
-#         #     # classes: B[classes]
-#         #     boxes = torch.cat(
-#         #         [t.view(n_batches, -1, 4) for t in boxes_by_feature], dim=1
-#         #     )
-#         #     classes = torch.cat(
-#         #         [t.view(n_batches, -1, len(CLASSES)) for t in classes_by_feature], dim=1
-#         #     )
-#         #     scores, classes, boxes = _gather_detections(
-#         #         boxes, classes, self.max_detections
-#         #     )
-#         #     return scores, classes, boxes
-
-#         # losses = []
-
-
-# def _feature_loss_targets(labels, features, img_height, img_width, stride):
-#     """
-#     Takes a feature map for a single layer of the FPN
-#     """
-
-
 def _gather_detections(classes, boxes, max_detections=DEFAULT_MAX_DETECTIONS):
-    # print("INPUT TO GATHER, b", boxes.shape)
-    # print("INPUT TO GATHER, c", classes.shape)
-
     # classes: BHW[c] class index of each box
     class_scores, class_indices = torch.max(classes.sigmoid(), dim=2)
 
@@ -138,8 +100,6 @@ def _gather_detections(classes, boxes, max_detections=DEFAULT_MAX_DETECTIONS):
     for i in range(n_batches):
         non_background_points = class_indices[i] > 0
 
-        print("non-background found", non_background_points.nonzero().shape)
-
         class_scores_i = class_scores[i][non_background_points]
         boxes_i = boxes[i][non_background_points]
         class_indices_i = class_indices[i][non_background_points]
@@ -151,20 +111,11 @@ def _gather_detections(classes, boxes, max_detections=DEFAULT_MAX_DETECTIONS):
         top_classes_i = torch.index_select(class_indices_i, 0, top_detection_indices)
         top_scores_i = torch.index_select(class_scores_i, 0, top_detection_indices)
 
-        # print("top_boxes_i", top_boxes_i.shape)
-        # print("top_classes_i", top_classes_i.shape)
-        # print("top_scores_i", top_scores_i.shape)
-
-        # print("BEFORE filter", top_boxes_i.shape)
         boxes_to_keep = torchvision.ops.nms(top_boxes_i, top_scores_i, 0.6)
 
         top_boxes_i = top_boxes_i[boxes_to_keep]
         top_classes_i = top_classes_i[boxes_to_keep]
         top_scores_i = top_scores_i[boxes_to_keep]
-
-        # print("top_boxes_i", top_boxes_i.shape)
-        # print("top_classes_i", top_classes_i.shape)
-        # print("top_scores_i", top_scores_i.shape)
 
         boxes_by_batch.append(top_boxes_i)
         classes_by_batch.append(top_classes_i)
